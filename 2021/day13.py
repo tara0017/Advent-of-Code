d9460fc4faf36d76c91c4df02baadb4b5c7c9094
tara0017 <tara0017@tc.umn.edu>
2021-12-15 19:56:27 -0500

    Add files via upload

---
+++ 2021/day13.py
@@ -0,0 +1,84 @@
+# day 13
+
+def fold_up(y):
+    global dots
+    dots_to_move = []
+    
+    for d in dots:
+        if d[1] > y:    #if the point is below the line
+            dots_to_move.append(d)
+
+    for d in dots_to_move:
+        dots.remove(d)
+        temp = (d[0], d[1] - 2*(d[1] - y))
+        dots.add(temp)
+
+
+def fold_left(x):
+    global dots
+    dots_to_move = []
+    
+    for d in dots:
+        if d[0] > x:    #if the point is left of the line
+            dots_to_move.append(d)
+
+    for d in dots_to_move:
+        dots.remove(d)
+        temp = (d[0] - 2*(d[0] - x), d[1])
+        dots.add(temp)
+
+
+
+        
+dots = set()
+instructions = []
+grid = []
+
+
+f = open('day13.txt', 'r')
+fold_inst = False
+for x in f:
+    if x == '\n':
+        fold_inst = True
+        continue
+    if fold_inst:
+        x = x.strip()
+        x = x.split()
+        x = x[2].split('=') 
+        instructions.append(x)
+    else:
+        x = x.strip()
+        x = x.split(',')
+        x = (int(x[0]), int(x[1]))
+        dots.add(x)
+
+
+for i in instructions:
+    if i[0] == 'y':
+        fold_up(int(i[1]))
+    else:
+        fold_left(int(i[1]))
+    
+
+
+
+for y in range(6):
+    for x in range(40):
+        if (x,y) in dots:
+            print('#', end = '')
+        else:
+            print(' ', end = '')
+    print()
+
+
+
+
+
+
+
+
+
+
+            
+
+
